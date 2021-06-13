422efbc1e1be27bb3b0df0bc6b0fe7865656dde4
Aamir Shehzad <aamir@nclouds.com>
2021-06-13 06:46:33 +0500

    changing root resource url

--- jenkins/groovy/resourceRoot-url.groovy
+++ jenkins/groovy/resourceRoot-url.groovy
@@ -1,6 +1,6 @@
 import jenkins.model.JenkinsLocationConfiguration
 import hudson.model.Descriptor
 jlc = JenkinsLocationConfiguration.get()
-jlc.setUrl("https://build.test-something.com/") 
+jlc.setUrl("http://build.test-something.com/") 
 jlc.save()
 println(jlc.getUrl())
