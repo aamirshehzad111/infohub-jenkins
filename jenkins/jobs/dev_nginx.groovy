39f0af59cd6fbd6a11a3726b12888c94eaf4aac0
Aamir Shehzad <aamir@nclouds.com>
2021-06-13 10:05:31 +0500

    setting up example job

--- jenkins/jobs/dev_nginx.groovy
+++ jenkins/jobs/dev_nginx.groovy
@@ -14,9 +14,13 @@ multibranchPipelineJob('Dev/nginx-deployment') {
         }
     }
     configure {
+        it / 'triggers' << 'com.cloudbees.hudson.plugins.folder.computed.PeriodicFolderTrigger' {
+            spec '* * * * *'
+            interval "60000"
+        }
         it / factory(class: "org.jenkinsci.plugins.workflow.multibranch.WorkflowBranchProjectFactory") << {
             scriptPath("pipelines/dev.groovy")
-        }     
+        }
     }
     orphanedItemStrategy {
         discardOldItems {
